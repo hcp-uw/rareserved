c36f56b433d534ba7fccd10a92c340ddf8efed90
Helena Kaikkonen <helenak@uw.edu>
2024-04-03 19:03:07 -0700

    working implementation for helena

--- starter-frontend/src/index.tsx
+++ starter-frontend/src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
