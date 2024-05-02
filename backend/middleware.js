d0a045dd6efc33817fe52c1bae75bbf18e4ff87d
Zack <zacktcrouse@gmail.com>
2024-05-01 17:50:18 -0700

    Moved over from other repo

--- backend/middleware.js
+++
@@ -1,6 +0,0 @@
-// define middleware to handle requests made to unknown endpoint
-const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
-}
-
-module.exports = { unknownEndpoint }
