74187f35143bbf74c4fd162c770190f803f86f00
Colin Lim <colinlim@uw.edu>
2024-05-06 13:27:02 -0700

    add default headings and body text styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        navbar: "#002E40",
         btn: {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
