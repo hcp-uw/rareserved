f250841fec8b12091e348a32d53bf8a337bd88a9
Helena Kaikkonen <helenak@uw.edu>
2024-05-08 19:01:13 -0700

    Implement Partial Divider and its color and spacing

---
+++ components/PartialDivider.tsx
@@ -0,0 +1,3 @@
+export default function PartialDivider() {
+    return(<hr className="w-64 bg-accent mt-48 mb-70"></hr>);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,16 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         navbar: "#002E40",
+        accent: "#F7B691",
         btn: {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
         },
       },
+      spacing: {
+        '48': '3rem',
+        '70': '4.375rem',
+      }
     },
   },
   plugins: [],
