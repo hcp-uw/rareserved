20d4e9467ffe68eab2a224b65ec59dfb1f34a6e4
Colin Lim <colinlim@uw.edu>
2024-05-20 14:07:04 -0700

    add new font + fix org organzation

--- components/Organization.tsx
+++ components/Organization.tsx
@@ -1,24 +1,28 @@
 import TEMPLOGO from '@/images/TEMPLOGO.png';
 import PLACEHOLDER from '@/images/orgPlaceholder.png';
-import { StaticImageData } from 'next/image';
 
 /** Creates an organization component with the given image, title, and header.
  * 
- * @param TODO
+ * @param _props - container for organization information
+ * @param _props.content - string description of the organization
+ * @param _props.title - string name of the organization
+ * @param _props - string image path of the logo
  * 
  * @returns Leftside square image with org title and description
  */
 export default function Organization(_props : {content:string, title:string, imgPath:string}) {
+  // TODO: change this image placeholder and find a better way
   let img = PLACEHOLDER.src;
   if (_props.imgPath==="TEMPLOGO") { img = TEMPLOGO.src; }
-  // TODO: find another way to load images, or update with logos
   if (_props.imgPath==="PLACEHOLDER") { img = PLACEHOLDER.src; }
   return (<div className="pb-70">
-    {/* TODO: fix the padding margins thing leftside*/}
-    <h3 className="text-left ml-24 pl-1 mb-2">{_props.title}</h3>
-    <div className="flex">
-      <img src={img} className="max-w-20 max-h-20 flex-1 mr-5"></img>
-      <p>{_props.content}</p>
+    <div className="flex-grow grid grid-cols-[15%_80%]">
+      <img src={img} className="max-w-20 max-h-20"></img>
+      <div className="ml-30">
+        <h3 className="">{_props.title}</h3>
+        <div className="max-w-20 max-h-20 mt-9"></div>
+        <p>{_props.content}</p>
+      </div>
     </div>
     </div>);
 }
