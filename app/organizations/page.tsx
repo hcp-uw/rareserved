bf896ac9dfe488c10bf173b126a3d2a6775bfaf6
Colin Lim <colinlim@uw.edu>
2024-05-23 12:18:44 -0700

    fixed dynamic image loading

--- app/organizations/page.tsx
+++ app/organizations/page.tsx
@@ -12,11 +12,11 @@ export default async function ProtectedPage() {
   <h1>Organizations</h1>
   <PartialDivider/>
   <Organization title="Name of Organization" content="Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. "
-   imgPath="../images/TEMPLOGO.png"/>
+   imgName="TEMPLOGO.png"/>
    <Organization title="Name of Organization" content="Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. "
-   imgPath=""/>
+   imgName=""/>
    <Organization title="Name of Organization" content="Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. "
-   imgPath=""/>
+   imgName=""/>
   </div>
     </>);
 }
--- components/Organization.tsx
+++ components/Organization.tsx
@@ -1,36 +1,26 @@
-import TEMPLOGO from '@/images/TEMPLOGO.png';
-import PLACEHOLDER from '@/images/orgPlaceholder.png';
-
-
-interface image {
-  type: string;
-}
-
 /** Creates an organization component with the given image, title, and header.
  * 
- * @param _props - container for organization information
- * @param _props.content - string description of the organization
- * @param _props.title - string name of the organization
- * @param _props - string image path of the logo
+ * @param props - container for organization information
+ * @param props.content - string description of the organization
+ * @param props.title - string name of the organization
+ * @param props.imgName - string image name of the logo (Must include type: .png, .jpg, etc.)
  * 
  * @returns Leftside square image with org title and description
  */
-export default function Organization(_props : {content:string, title:string, imgPath:string}) {
-  // TODO: change this image placeholder and find a better way
-  // let img = PLACEHOLDER.src;
-  let img = _props.imgPath;
-  if (_props.imgPath==="TEMPLOGO") { img = TEMPLOGO.src; }
-  // const logo:image = require('../images/TEMPLOGO.png');
-  return (<div className="pb-70">
-    
-    <img src={"../images/TEMPLOGO.png"} />
-    <div className="grid grid-cols-[15%_80%] ml-10px">
-      <div style={{ backgroundImage: "url(/TEMPLOGO.src)" }} className="max-w-20 max-h-20"/>
-      <div className="ml-30">
-        <h3 className="">{_props.title}</h3>
-        <div className="max-w-20 max-h-20 mt-9"></div>
-        <p>{_props.content}</p>
+export default function Organization(props : {content:string, title:string, imgName:string}) {
+  let img = props.imgName;
+  if (img === "") {
+    img = "/images/orgPlaceholder.png"
+  } else { 
+    img = "/images/" + img;
+  }
+  return (<div className="pb-70">    
+    <div className="grid grid-cols-[auto_80%]">
+      <div style={{ backgroundImage: `url(${img})` }} className="max-w-133px max-h-133px bg-contain bg-no-repeat"/>
+        <div className="pl-15 max-h-133px">
+          <h3 className="">{props.title}</h3>
+          <p>{props.content}</p>
+        </div>
       </div>
-    </div>
     </div>);
 }
