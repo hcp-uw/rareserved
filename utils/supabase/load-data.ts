f3761ac324c5677cb28fce00d8c4ad539bf345ae
zcrouse <zcrouse@cs.washington.edu>
2024-06-26 18:40:50 -0700

    Added called to supabase

--- utils/supabase/load-data.ts
+++ utils/supabase/load-data.ts
@@ -1,8 +1,19 @@
 import { RSEvent, RSOrganizations, RSBlog } from "../data-types";
+import { createClient } from "@/utils/supabase/server";
+
+const supabase = createClient();
 
 export const getEvents = async (amount: number) : Promise<RSEvent[]> => {
+    const { data, error } = await supabase
+    .from('Test')
+    .select('*')
 
+    console.log(data);
 } 
+
+export const setEvent = async (id: number, newEvent: RSEvent) : Promise<RSEvent[]> => {
+
+}
 
 export const getOrgs = async (amount: number) : Promise<RSOrganizations[]> => {
 
