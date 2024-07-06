1453c5b71162a5adeebcdd73a4f5f8129a211f6f
zcrouse <zcrouse@cs.washington.edu>
2024-07-06 14:04:40 -0700

    Added blogs and organization upsert functions

--- utils/supabase/update-data.ts
+++ utils/supabase/update-data.ts
@@ -2,6 +2,7 @@ import { SupabaseAuthClient } from "@supabase/supabase-js/dist/module/lib/Supaba
 import { RSEvent, RSOrganization, RSBlog, makeEvent } from "../data-types";
 import { createClient } from "@/utils/supabase/server";
 import { SupabaseClient } from "@supabase/supabase-js";
+import { describe } from "node:test";
 
 export const upsertEvent = async (event: RSEvent, supabase: SupabaseClient) => {
     if (event.id == undefined) {
@@ -33,6 +34,76 @@ export const upsertEvent = async (event: RSEvent, supabase: SupabaseClient) => {
                 address: event.address
             })
             .eq('id', event.id)
+
+            if (error != undefined) {
+                console.log("Updating Error: " + error.message);
+            }
+    }
+}
+
+export const upsertOrg = async (organization: RSOrganization, supabase: SupabaseClient) => {
+    if (organization.id == undefined) {
+        // Add new event to database
+        const { data, error } = await supabase
+            .from('Organizations')
+            .insert({
+                title: organization.title, 
+                subtitle: organization.subtitle,
+                link: organization.link,
+                description: organization.description
+            })
+
+            if (error != undefined) {
+                console.log("Updating Error: " + error.message);
+            }
+    } else {
+        // Update given event in database
+        const { data, error } = await supabase
+            .from('Organizations')
+            .update({
+                title: organization.title, 
+                subtitle: organization.subtitle,
+                link: organization.link,
+                description: organization.description
+            })
+            .eq('id', organization.id)
+
+            if (error != undefined) {
+                console.log("Updating Error: " + error.message);
+            }
+    }
+}
+
+export const upsertBlog = async (blog: RSBlog, supabase: SupabaseClient) => {
+    if (blog.id == undefined) {
+        // Add new event to database
+        const { data, error } = await supabase
+            .from('Events')
+            .insert({
+                title: blog.title, 
+                subtitle: blog.subtitle, 
+                picture: blog.picture,
+                author: blog.author,
+                text: blog.text,
+                date_posted: blog.date_posted
+            })
+
+            if (error != undefined) {
+                console.log("Updating Error: " + error.message);
+            }
+    } else {
+        // Update given event in database
+        const { data, error } = await supabase
+            .from('Events')
+            .update({
+                title: blog.title, 
+                subtitle: blog.subtitle, 
+                picture: blog.picture,
+                author: blog.author,
+                text: blog.text,
+                date_posted: blog.date_posted
+            })
+            .eq('id', blog.id)
 
             if (error != undefined) {
                 console.log("Updating Error: " + error.message);
