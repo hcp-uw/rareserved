baecf77f67165604d700cc0e860a0f2a6b07a66d
zcrouse <zcrouse@cs.washington.edu>
2024-08-22 19:01:47 -0700

    Removed Imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-//'use client';
+'use client';
 
 import DeployButton from "../components/tutorial/DeployButton";
 import AuthButton from "../components/tutorial/AuthButton";
@@ -10,8 +10,7 @@ import NavigationBar from "@/components/NavigationBar";
 import TEMPLOGO from '@/images/TEMPLOGO.png';
 import FullDivider from "@/components/FullDivider";
 import Footer from "@/components/Footer";
-import Counter from "@/components/testButton";
-import blog from "@/components/testButton";
+import blog from "@/components/TestButton";
 import { getEvents } from "@/utils/supabase/load-data";
 import { upsertEvent } from "@/utils/supabase/update-data";
 import { makeEvent } from "@/utils/data-types";
@@ -30,7 +29,6 @@ export default async function Index() {
 
   upsertEvent(newEvent, supabase);
   console.log('Just Updated')
-
   return (
     <>
       <NavigationBar/>
@@ -40,7 +38,6 @@ export default async function Index() {
         <p className="">Over 7000 rare diseases impact people worldwide and have insufficient research and treatment available due to how infrequently these conditions occur. Historically, research has placed time and resources into topics that have profit, repeatedly underrepresenting rare diseases. For rare disease patients, they often do not receive a diagnosis for their disease, let alone be supported by treatment that makes a difference. RAREserved seeks to amplify voices to make knowledge about rare diseases accessible and engaging by telling real stories of those impacted by a rare disease. RAREserved conducts interviews and creates a narrative article that shares an individual's experience. We seek to educate our audience on rare disease topics and advocate for change and awareness of rare diseases as a marginalized community in healthcare. RAREserved recognizes the importance of diversity, equity, and inclusion in healthcare and research. Our mission is to raise awareness and knowledge for the pursuit of better change.</p>
       </div>
       <FullDivider/>
-      <Counter/>
       <Footer/>
     </>
   );
