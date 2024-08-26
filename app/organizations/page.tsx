591691075f2b028b4edc5d641ab440470dce1e01
zcrouse <zcrouse@cs.washington.edu>
2024-08-26 14:47:46 -0700

    Added code to get orgs from database

--- app/organizations/page.tsx
+++ app/organizations/page.tsx
@@ -1,22 +1,22 @@
 import NavigationBar from "@/components/NavigationBar";
 import Organization from "@/components/Organization";
 import PartialDivider from "@/components/PartialDivider";
+import { RSOrganization } from "@/utils/data-types";
+import { getOrgs } from "@/utils/supabase/load-data";
 
 
 export default async function ProtectedPage() {
 
+  const orgs : RSOrganization[] = await getOrgs();
+
+  const orgsComponents = orgs.map((org, index, __) => Organization(org, index));
 
   return (<>
   <NavigationBar/>
   <div id="main">
   <h1>Outreach, Diversity, and Inclusion</h1>
   <PartialDivider/>
-  <Organization title="Name of Organization" content="Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. "
-   imgName="TEMPLOGO.png"/>
-   <Organization title="Name of Organization" content="Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. "
-   imgName=""/>
-   <Organization title="Name of Organization" content="Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. "
-   imgName=""/>
+  {orgsComponents}
   </div>
     </>);
 }
