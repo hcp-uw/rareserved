878ace7788659ea0013a2734f338b5aade0427cf
Zack <zacktcrouse@gmail.com>
2025-01-14 18:16:41 -0800

    Commented out old code breaking the website.

--- app/specificpost/page.tsx
+++ app/specificpost/page.tsx
@@ -2,7 +2,7 @@ import NavigationBar from "@/components/NavigationBar";
 import SpecificPost from "@/components/SpecificPost";
 
 
-export default async function ProtectedPage() {
+/*export default async function ProtectedPage() {
 
 
   return (<>
@@ -10,7 +10,7 @@ export default async function ProtectedPage() {
   <div id="main">
     <SpecificPost author="Tom Billington" 
     title="Title of a Specific Post" 
-    imgName="TEMPLOGO.png" 
+    imgName ="TEMPLOGO.png" 
     date = "Janurary 1st, 2024"
     content="“Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. His heart was heavy with a sorrow that the town's folks could not comprehend. For Tom, the world was a canvas of grey, the colors of joy and happiness seemed to have faded away. His mother tried to cheer him up, with toys, candies, and tales of magic and wonder,”
 “Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. His heart was heavy with a sorrow that the town's folks could not comprehend. For Tom, the world was a canvas of grey, the colors of joy and happiness seemed to have faded away. His mother tried to cheer him up, with toys, candies, and tales of magic and wonder,”
@@ -24,4 +24,4 @@ export default async function ProtectedPage() {
 “Once upon a time in a small town lived a boy named Tom. Tom was always sad. He would sit by the riverbank every day, gazing at the water and the ducks, his eyes filled with a profound melancholy that no child his age should bear. His heart was heavy with a sorrow that the town's folks could not comprehend. For Tom, the world was a canvas of grey, the colors of joy and happiness seemed to have faded away. His mother tried to cheer him up, with toys, candies, and tales of magic and wonder,”"/>
   </div>
     </>);
-}
+}*/
