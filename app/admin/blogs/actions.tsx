8233884fe380f7d5fc84158fc65114e627aaafcc
zcrouse <zcrouse@cs.washington.edu>
2025-02-14 00:52:49 -0800

    Removed print statements for debugging

--- app/admin/blogs/actions.tsx
+++ app/admin/blogs/actions.tsx
@@ -3,7 +3,5 @@ import { RSBlog } from "@/utils/data-types";
 import { deleteBlog } from "@/utils/supabase/update-data";
 
 export const handleDelete = async (blog: RSBlog) => {
-    console.log('deleting blog: ');
-    console.log(blog)
     await deleteBlog(blog);
 }
