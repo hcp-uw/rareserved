65987513b5ad404515e4b75eee9f2dbb86d2199d
zcrouse <zcrouse@cs.washington.edu>
2025-02-19 20:40:43 -0800

    Removed commented out code

--- components/admin/EditBlogChunk.tsx
+++ components/admin/EditBlogChunk.tsx
@@ -10,17 +10,6 @@ import { handleDelete } from "@/app/admin/blogs/actions";
  * @param props.author Name of author
  * @returns Blog chunk component
  */
-// export default function EditBlogChunk(blog : RSBlog, key : Number, supabase: SupabaseClient) {
-
-//     // TODO: if clicked, redirect("blog?id=123456") or smth????
-//     // return(<div className="min-w-200px max-w-200px shrink-0">
-//     return(<div className="w-full space-y-[10px]" key= {key.toString()}>
-//         {BlogChunk(blog, key)}
-//         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" 
-//             onClick={() => deleteBlog(blog, supabase)}>Delete</button>
-//     </div>);
-// }
-
 interface EditBlogChunkProps {
     blog: RSBlog;
     index: Number;
