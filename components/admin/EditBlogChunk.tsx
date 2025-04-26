476e8e85dec1796cca445828dc62ceb306ad12bb
zcrouse <zcrouse@cs.washington.edu>
2025-04-25 21:47:23 -0700

    Added create blog button and cancel edit blog button

--- components/admin/EditBlogChunk.tsx
+++ components/admin/EditBlogChunk.tsx
@@ -19,7 +19,7 @@ const EditBlogChunk: React.FC<EditBlogChunkProps> = ({blog, index}) => {
     const deleteCallback = async () => await handleDelete(blog);
     return(<div key= {index.toString() + " with button"}>
         {BlogChunk(blog, index)}
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" 
+        <button className="text-white mt-2 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" 
             onClick={deleteCallback}>Delete</button>
     </div>);
 }
