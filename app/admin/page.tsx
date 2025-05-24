666c97da9d36a713ac6d34719e36f018cb7984e3
Helena Kaikkonen <helenak@uw.edu>
2025-05-24 00:20:07 -0700

    Update edit buttons, add logout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 import PartialDivider from '@/components/PartialDivider'
+import LogoutText from '@/components/admin/LogoutText'
+import { logout } from '../login/actions'
 
 export default async function PrivatePage() {
   const supabase = await createClient()
@@ -14,12 +16,10 @@ export default async function PrivatePage() {
     <div id="main" className="text-center">
       <h1>Hello Admin!</h1>
       <PartialDivider />
-      <button className="mt-5 p-1 bg-darkblue text-white w-80 rounded-full"><a href={'/admin/blogs'}>Edit Blogs</a></button>
-      <br />
-      <button className="mt-5 mb-10 p-1 bg-darkblue text-white w-80 rounded-full"><a href={'/admin/events'}>Edit Events</a></button>
-      <br />
-      <a className="mx-auto cursor-pointer block w-fit mb-4" href="/">Back to regular website</a>
-      {/* TODO implement logout?: <a className="mx-auto cursor-pointer block w-fit" onClick={logout}>Log out</a> */}
+      <a className="mx-auto mt-5 p-1 bg-darkblue text-white block w-80 rounded-full hover:no-underline" href={'/admin/blogs'}>Edit Blogs</a>
+      <a className="mx-auto mt-5 p-1 bg-darkblue text-white block w-80 rounded-full hover:no-underline" href={'/admin/events'}>Edit Events</a>
+      <a className="mx-auto mt-10 cursor-pointer block w-fit mb-4" href="/">Back to regular website</a>
+      <LogoutText />
     </div>
   </>
 }
