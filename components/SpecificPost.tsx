5eb034bd2ed23cdf15766cccb6b2ffb9af6db102
klg42 <klg42@uw.edu>
2025-05-27 16:59:30 -0700

    Fixed floating footer + specific past new lines. I also copied colins footer sorry...

--- components/SpecificPost.tsx
+++ components/SpecificPost.tsx
@@ -1,4 +1,10 @@
 import { RSBlog } from "@/utils/data-types";
+import { error } from "console";
+
+function changeText(text: string): string {
+  console.log(text);
+  return "hi";
+}
 
 export default function FullBlogPost(blog : RSBlog, key : Number) {
   let img = "/images/";
@@ -7,6 +13,7 @@ export default function FullBlogPost(blog : RSBlog, key : Number) {
   } else {
     img += blog.picture
   }
+
 
   const formattedDate = blog.date_posted.toLocaleDateString("en-US", {
     month: "long",
@@ -18,16 +25,18 @@ export default function FullBlogPost(blog : RSBlog, key : Number) {
 
   // TODO: Add check to see if image exists, if not use a default image.
   // TODO: Change .toISOString string later
-  return (<div key= {key.toString()}>   
-      <span className="flex"> 
+  return (<div key= {key.toString()}>
+      <span className="flex">
         <p className="mr-10 font-bold"> {blog.author} </p>
         <p className="font-normal"> {formattedDate} </p>
       </span>
       <h1 className="text-left">{blog.title}</h1>
       <h3 className="text-left">{blog.subtitle}</h3>
       <img src={img} className="w-full h-full max-h-[30vh] object-scale-down bg-cover bg-center bg-no-repeat my-8" ></img>
-      <div className="pl-15 max-h-133px">
-        <p className="pb-70px">{blog.text}</p>
+      <div className="pl-15 max-h-700px">
+        <p className="pb-70px whitespace-pre-wrap">{blog.text}</p>
       </div>
     </div>);
 }
+
+
